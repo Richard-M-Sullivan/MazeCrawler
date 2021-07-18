68d34ff0199f51a30eeebe7f9c5a3dbaf7f6419a
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2021-07-18 18:47:33 -0500

    added documentation to the server chat and description files

--- client/maze-crawler-frontend/src/components/Chat.js
+++ client/maze-crawler-frontend/src/components/Chat.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import socket from '../services/socketService';
-import ReactScrollableFeed from 'react-scrollable-feed';
+import socket from '../services/socketService'; // this allows us to listen to socket events with the global socket obj
+import ReactScrollableFeed from 'react-scrollable-feed'; // this allows us to make a scrollable chat box
 import SubmitBar from './SubmitBar';
 
+// This is a component provides a window to view and read chats as well as a submit bar to send chats to all 
+// other users in the server
 
+// This works by having the submit bar send a chat message event to the server through sockets. Then the server handles
+// the event and will send a chat message event to all clients connected to the server (including the chat sender).
+// This is handled by displaying the chat message in the chat window.
 export default function Chat(props){
 
+    // dummy data to add to the chatData
     const [chatData,setChatData] = React.useState([
         {user:"richard",content:"hello conner!"},
         {user:"conner",content:"how goes it buddy? :^)"},
@@ -15,8 +21,10 @@ export default function Chat(props){
         {user:"david",content:"askja sluff salk las kef laski d lugsail s skuld flog jbht pdfjs lbj"}
     ]);
     
+    // chats are the ui elements that get rendered. It starts off empty.
     const [chats,setChats] = React.useState([]);
 
+    // this adds a chat message and user to the chatData state
     const addChat = function ({user,content}) {
         if(content != null){
             //console.log("adding chat");
@@ -25,14 +33,17 @@ export default function Chat(props){
         }
     };
 
+    // this emits a chat message event to the server with the relevant information.
     const submitMessage = function(chatobj){
         //console.log(`emitting ${chatobj}`);
         socket.emit("chat message",chatobj);
     }
 
+    // this runs on the first render and it specifies how to handle incoming chat events from the server
     React.useEffect(()=>{
         //console.log("does this get run more than once");
         
+        // create a chat event listener that will call add chat and pass on the chat message from the server
         socket.on("chat message",(message)=>{
             //console.log("chat received");
             addChat(message);
@@ -40,10 +51,13 @@ export default function Chat(props){
 
     },[]);
 
+    // this builds the ui and is executed every time that the chatData state is changed. It will then
+    // set all the information of chats based on chatData and then chats is what is rendered to the ui
     React.useEffect(()=>{
 
         //console.log(chatData);
 
+        // for every element in chatData state create a ui element and add that element to chats state
         setChats(
             chatData.map(({user,content},index)=>{
                 return(
@@ -61,6 +75,7 @@ export default function Chat(props){
     },[chatData]);
 
 
+    // this returns the ui and is returned every re-render
     return (
         <div className="chat flexv">
             <div className="hasBorder hasBackground flexv flexCenter overflowHidden">
--- client/maze-crawler-frontend/src/components/Description.js
+++ client/maze-crawler-frontend/src/components/Description.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import socket from '../services/socketService';//grants access to the global socket obj to send and handle messages
-import ReactScrollableFeed from 'react-scrollable-feed';
+import ReactScrollableFeed from 'react-scrollable-feed';//is what allows me to make a scrollable message box.
 import SubmitBar from './SubmitBar';
 
+// The description component provides a window to display information pertaining to the game and contains a submit bar
+// that allows you to submit commands to the server for the purpose of getting back messages to be displayed in the display
+// pertaining to the game.
+
+// This works by having the submit bar send a description message event to the server through sockets. Then the server handles
+// the event and will send back a description message event to the client. This is handled by displaying the message in the
+// description window. 
 export default function Description(props){
 
+    // create dummy data to be displayed 
     const [descriptionData,setDescriptionData] = React.useState([
         {content:`You wake up in a forest. All around you hear the rustling of trees, and up ahead you hear falling water.
         You have a gash in your side and an empty canteen. What do you do, weary traveller?`},
@@ -18,23 +26,31 @@ export default function Description(props){
         that is still burning. Suddenly you get the feeling as though you are being watched.`}
     ]);
     
+    // ui elements initially set to empty
     const [descriptions,setDescriptions] = React.useState([]);
 
+    // this adds an element to descriptionData state
     const addDescription = function ({content}) {
-        console.log("adding description");
+        //console.log("adding description");
         if(content != null){
             setDescriptionData(descriptionData=>[...descriptionData,{content:content}]);
         }
     };
 
+    // this sends a socket message to the server containing a message. this is passed to
+    // the submit bar to be called after you type in and submit a command.
     const submitMessage = function(descriptionobj){
-        console.log(`emitting ${descriptionobj}`);
+        //console.log(`emitting ${descriptionobj}`);
         socket.emit("description message",descriptionobj);
     };
 
+    // this runs only once on the initial render
+    // it sets up the socket message listener
     React.useEffect(()=>{
-        console.log("does this get run more than once");
+        //console.log("does this get run more than once");
         
+        // this listens for a message event of type description message from the server, and when
+        // it is activated it calls add description and hands to it the data that it needs to append a message to displayData
         socket.on("description message",(message)=>{
             console.log("description received");
             addDescription(message);
@@ -42,8 +58,12 @@ export default function Description(props){
 
     },[])
 
+    // this runs every time descriptionData is updated
+    // it builds the user interface and stores it in the state variable to be displayed in the ui
     React.useEffect(()=>{
-        console.log(descriptionData);
+        //console.log(descriptionData);
+
+        // go through descriptionData and make a ui element for each data point
         setDescriptions(
             descriptionData.map(({content},index)=>{
                 return(
@@ -60,6 +80,7 @@ export default function Description(props){
 
     },[descriptionData]);
 
+    //return the html needed to build the site. This is run every re-render
     return (
         <div className="description flexv">
             <div className="content hasBorder hasBackground flexv flexCenter overflowHidden">
