9bc9bb04a0d5b45648cb06c58aff90bf9331a89f
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2021-08-08 10:57:01 -0500

    completed login and add user actions

--- server/serverApp.js
+++ server/serverApp.js
@@ -1,35 +1,30 @@
-const MongoClient = require("mongodb").MongoClient;
 
-const uri = "mongodb://localhost:27017";
+  ////////////////////////
+ // setting up mongodb //
+////////////////////////
 
+const MongoClient = require("mongodb").MongoClient;//this imports all the needed things to run the database
 
-const client = new MongoClient(uri);
+const uri = "mongodb://localhost:27017";//the uri string defines the connection that I want to make with the server
+const mongo = new MongoClient(uri);// create the client object with the connection string.
 
-async function run(){
-    await client.connect();
+mongo.connect(); // connect to server
 
-    const database = client.db("MazeCrawler");
-    const collection = database.collection("userInfo");
-    const userCursor = await collection.find({});
-    const users = await userCursor.toArray();
+// mongo is my hook to the database. To access things you have to set the database and collection, and then perform
+// an action. mongo.db("exampleDatabase").collection("exampleCollection").doStuff();
 
-    console.log(users);
-
-
-    client.close();
-}
-run()
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+  ///////////////////////////////////////
+ // setting up the server and sockets //
+///////////////////////////////////////
 
 const Server = require("http"); //this will allow us to make a server object that can be used to make the socket connection and express stuff
 const express = require("express");// this will allow us to utilize express server api for handling requests
 const Socket = require("socket.io");// this allows us to utilize sockets
 
 const port = 5000;//this is the port that we will be listening to
-
-let ban = "";
 
 //create an express object, which is the information needed to make a server.
 //after that make a server object as specified by our app obj
@@ -50,20 +45,23 @@ const io = Socket(server,{
 // have the server that socket and app are linked to start listening to incoming traffic
 server.listen(`${port}`);
 
-//when a computer connects create the needed listeners to respond to the incoming requests
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  ///////////////////////////////
+ // logic for the application //
+///////////////////////////////
+
+//when a computer connects, create the needed listeners to respond to the incoming requests
 io.on("connection",(socket)=>{
 
     //log when a connection happens
     console.log("socket connection established!");
 
-
-
     // when someone disconnects log the disconnection
     socket.on('disconnect',()=>{
         console.log('disconnected');
     });
-
-
 
     //this takes in a chat message and will relay that chat to everyone connected to the server
     socket.on("chat message",(message) =>{
@@ -78,12 +76,8 @@ io.on("connection",(socket)=>{
         else{
 
             io.emit("chat message",message);
-
         }
-
     });
-
-
 
     //this takes in a description message and will relay that message back to the user
     socket.on("description message",(message)=>{
@@ -129,30 +123,73 @@ io.on("connection",(socket)=>{
                 //if they are setting their username
                 if(message.user === ""){
                     socket.emit("set username",{state:message.content});
-                    socket.emit("description message",{content:"username set - no checking just setting username to what you typed in"});
                     socket.emit("description message",{content:"enter your password"});
                 }
                 //if they need to ser their password
                 else{
-                    socket.emit("set validated",{state:true});
-                    socket.emit("description message",{content:"password taken in, but no checks just setting validated to true"});
-                    socket.emit("description message",{content:"you are now logged in"});
+                    //if the username and password are correct, then validate the user and let them know they are logged in
+                    //if the username and password are incorrect, then reset user info and lett them know to try again
+                    (async () => {
+                        //set user and password variables
+                        let user = message.user;
+                        let pass = message.content;
 
+                        //look to see if this person exists in the database
+                        let find = await mongo.db("MazeCrawler").collection("userInfo").find({userName:user,userPassword:pass});
+
+                        let userArray = await find.toArray();
+                        console.log(userArray);
+
+                        //if you find a record then success
+                        if(await find.count() == 1){
+                            //validate the user
+                            socket.emit("set validated",{state:true});
+                            //tell the ser they are logged in
+                            socket.emit("description message",{content:"you are now logged in."});
+
+                        }
+                        //if not, then unsuccessful
+                        else{
+                            //reset user name to null and validated to false
+                            socket.emit("set validated",{state:false});
+                            socket.emit("set username",{state:null});
+                            //tell the user to try logging in again
+                            socket.emit("description message",{content:"username and/or password incorrect. type login to try again."});
+                        }
+                    })();
                 }
             }
+
             //if they are in the process of creating a new user
             else if(message.validated === ""){
                //if they are setting their username
-               if(message.user === ""){
-                socket.emit("set username",{state:message.content});
-                socket.emit("description message",{content:"username set - no checking just setting username to what you typed in"});
-                socket.emit("description message",{content:"enter your password"});
+                if(message.user === ""){
+                    //if the username exists, then do nothing and let user know to re-try a username
+                    //if the username does not exist, then set the username and add it to the database and let the user know to enter a password
+                    (async ()=>{
+
+                        //look to see if a user with the specified name is in the database
+                        let user = message.content;
+                        let find = await mongo.db("MazeCrawler").collection("userInfo").find({userName:user});
+
+                        //a user of the specified name has been found in the database
+                        if(await find.count() == 1){
+                            socket.emit("description message",{content:"this username already exists. try again..."});
+                        }
+                        else{
+                            socket.emit("set username",{state:message.content});
+                            socket.emit("description message",{content:"enter your password"});
+                        }
+
+
+                    })();
                 }
-                //if they need to ser their password
+                //if they need to set their password
                 else{
                     socket.emit("set validated",{state:true});
-                    socket.emit("description message",{content:"password taken in, but no checks just setting validated to true"});
                     socket.emit("description message",{content:"you are now logged in"});
+
+                    mongo.db("MazeCrawler").collection("userInfo").insertOne({userName:message.user,userPassword:message.content});
 
                 } 
             }
@@ -174,5 +211,5 @@ app.get("/",(req,res)=>{
     console.log("hello");
 });
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
